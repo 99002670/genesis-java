06f18df322bdfa688550c9c71666e5bcffc689a5
Milind Mohapatra <milind.mohapatra@ltts.com>
2020-10-14 15:35:56 +0530

    Add data type demo code

---
+++ day1/Demo.java
@@ -0,0 +1,11 @@
+public class Demo
+{
+	public static void main(String args[])
+	{
+		float f = 3.14f;
+		boolean flag = true;
+		int arr[] = new int[10];
+		int len = arr.length;
+		System.out.println("Hello world!");
+	}
+}
